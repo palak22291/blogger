0225f6d4b65f87caf3cc7162fb338ada680b3e40
Palak Gupta <palakgupta@Palaks-MacBook-Pro-3.local>
2025-10-25 04:47:46 +0530

    sorting posts implemented

--- Backend/src/Controllers/postController.js
+++ Backend/src/Controllers/postController.js
@@ -34,28 +34,29 @@ exports.getAllPosts = async (req, res) => {
     const limit = parseInt(req.query.limit) || 5;
     const search = req.query.search || "";
     const skip = (page - 1) * limit;
-    const category = req.query.category || "" ;
-    const published= req.query.published 
+    const category = req.query.category || "";
+    const published = req.query.published;
+    const order = req.query.order === "asc" ? "asc" : "desc";
+    const sortBy = req.query.sortBy || "createdAt";
 
-    // filtering 
+    // filtering
 
-    const filterConditions={
-      AND:[
-        search ?{
-          OR:[
-            {title:{contains:search}},
-            {content:{contains:search}}
-          ]
+    const filterConditions = {
+      AND: [
+        search
+          ? {
+              OR: [
+                { title: { contains: search } },
+                { content: { contains: search } },
+              ],
+            }
+          : {},
+        category ? { category: category } : {},
+        published !== undefined ? { published: published === "true" } : {},
+      ],
+    };
 
-        } : {},
-        category ? {category: category} :{},
-        published !== undefined ? {published: published === "true"} : {}
-
-      ]
-    }
-
-
-      // now we will fecth posts with pagination and search
+    // now we will fecth posts with pagination and search and sorting 
     const posts = await prisma.post.findMany({
       where: filterConditions,
       include: {
@@ -66,8 +67,15 @@ exports.getAllPosts = async (req, res) => {
             email: true,
           },
         },
+        _count:{
+          select:{
+            likes:true,comments:true
+          }
+        }
+
       },
-      orderBy: { createdAt: "desc" },
+      orderBy: {[sortBy]: order },
+      // dynamic sorting using req.params
       skip,
       take: limit,
 
@@ -78,7 +86,7 @@ exports.getAllPosts = async (req, res) => {
     // Without count, you only get the current page’s posts and cannot know how many pages exist in total.
 
     const totalPosts = await prisma.post.count({
-      where: filterConditions
+      where: filterConditions,
     });
     res.json({
       page,
@@ -89,7 +97,7 @@ exports.getAllPosts = async (req, res) => {
     });
     // totalPosts + totalPages → metadata for pagination
     // posts → actual data to display
-     // Together, it makes your backend API pagination-ready and search-enabled.
+    // Together, it makes your backend API pagination-ready and search-enabled.
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "something went wrong" });
